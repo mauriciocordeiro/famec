2914baa0fe85ed297bbfb7f14ddfc59648c71820
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-05-23 11:44:44 -0300

    Initial commit

---
+++ src/main/java/br/org/mcord/famec/FamecApplication.java
@@ -0,0 +1,13 @@
+package br.org.mcord.famec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/mcord/famec/FamecApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.mcord.famec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
