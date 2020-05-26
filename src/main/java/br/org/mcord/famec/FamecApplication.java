5e5428f1dc8c75aa032fedeeb0e216dedce3b72c
Maurício Cordeiro <mauriciocordeiro@live.com>
2020-05-26 13:12:37 -0300

    Update FamecApplication.java

--- src/main/java/br/org/mcord/famec/FamecApplication.java
+++ src/main/java/br/org/mcord/famec/FamecApplication.java
@@ -1,11 +1,14 @@
 package br.org.mcord.famec;
 
+import java.util.Arrays;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -15,6 +18,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import br.org.mcord.famec.config.JWTAuthorizationFilter;
 
@@ -36,24 +40,31 @@ public class FamecApplication extends SpringBootServletInitializer {
 
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
+			http.cors().and().csrf().disable()
 				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
 				.authorizeRequests()
 				.antMatchers(HttpMethod.POST, "/api/login").permitAll()
 				.anyRequest().authenticated();
 			
-			http.cors().configurationSource(new CorsConfigurationSource() {
-	            @Override
-	            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-	                return new CorsConfiguration().applyPermitDefaultValues();
-	            }
-	        });
+//			http.cors().configurationSource(new CorsConfigurationSource() {
+//	            @Override
+//	            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//	                return new CorsConfiguration().applyPermitDefaultValues();
+//	            }
+//	        });
 		}
 		
-//		@Autowired
-//	    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
-//	        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-//	    }
+		@Bean
+	    CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration configuration = new CorsConfiguration();
+	        configuration.setAllowedOrigins(Arrays.asList("*"));
+	        configuration.setAllowedMethods(Arrays.asList("*"));
+	        configuration.setAllowedHeaders(Arrays.asList("*"));
+	        configuration.setAllowCredentials(true);
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", configuration);
+	        return source;
+	    }
 	}
 
 }
